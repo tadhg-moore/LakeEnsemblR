c3eb5e631760298960baad2e4c1866fe306e30a1
Tadhg Moore <tadhg.moore6@gmail.com>
2023-03-21 21:21:28 +0000

    Tidy up code

--- R/export_dirs.R
+++ R/export_dirs.R
@@ -28,79 +28,14 @@ export_dirs <- function(config_file, model = c("GOTM", "GLM", "Simstrat", "FLake
   # check model input
   model <- check_models(model)
   
-##---------------FLake-------------
-  if("FLake" %in% model){
-    # Create directory and output directory, if they do not yet exist
-    if(!dir.exists("FLake/output")){
-      dir.create("FLake/output", recursive = TRUE)
-    }
-    
-    # Read the FLake config file from config_file, and write it to the FLake directory
-    temp_fil <- get_yaml_value(config_file, "config_files", "FLake")
+  sapply(model, function(m) {
+    dir.create(paste0(m, "/output"), recursive = TRUE)
+    # Read the model config file from config_file, and write it to the model directory
+    temp_fil <- get_yaml_value(config_file, "config_files", m)
     if(!file.exists(temp_fil)){
-      get_template("FLake_config", folder = folder, filename = temp_fil)
+      get_template(template = paste0(m, "_config"), folder = folder, filename = temp_fil)
     }
-  }
-  
-##---------------GLM-------------
-  if("GLM" %in% model){
-    # Create directory and output directory, if they do not yet exist
-    if(!dir.exists("GLM/output")){
-      dir.create("GLM/output", recursive = TRUE)
-    }
-    
-    # Read the GLM config file from config_file, and write it to the GLM directory
-    temp_fil <- get_yaml_value(config_file, "config_files", "GLM")
-    
-    if(!file.exists(temp_fil)){
-      get_template("GLM_config", folder = folder, filename = temp_fil)
-    }
-  }
-  
-##---------------GOTM-------------
-  if("GOTM" %in% model){
-    # Create directory and output directory, if they do not yet exist
-    if(!dir.exists("GOTM/output")){
-      dir.create("GOTM/output", recursive = TRUE)
-    }
-    
-    # Read the GOTM config file from config_file, and write it to the GOTM directory
-    temp_fil <- get_yaml_value(config_file, "config_files", "GOTM")
-    if(!file.exists(temp_fil)){
-      get_template("GOTM_config", folder = folder, filename = temp_fil)
-    }
-  }
-  
-##---------------Simstrat-------------
-  if("Simstrat" %in% model){
-    # Create directory and output directory, if they do not yet exist
-    if(!dir.exists("Simstrat/output")){
-      dir.create("Simstrat/output", recursive = TRUE)
-    }
-    
-    # Read the Simstrat config file from config_file, and write it to the Simstrat directory
-    temp_fil <- get_yaml_value(config_file, "config_files", "Simstrat")
-    if(!file.exists(temp_fil)){
-      get_template("Simstrat_config", folder = folder, filename = temp_fil)
-    }
-  }
-  
-##---------------MyLake-------------
-  if("MyLake" %in% model){
-    # Create directory and output directory, if they do not yet exist
-    if(!dir.exists("MyLake")){
-      dir.create("MyLake")
-    }
-    
-    # Load config file MyLake
-    temp_fil <- get_yaml_value(config_file, "config_files", "MyLake")
-    if(!file.exists(temp_fil)){
-      get_template("MyLake_config", folder = folder, filename = temp_fil)
-      
-      # Load template config file from extdata
-      load(file.path(folder, temp_fil))
-    }
-  }
+  })
   
   message("export_dirs complete!")
 }
